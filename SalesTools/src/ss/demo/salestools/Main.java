e1247e0c6f35865c54f68e5b50024824f0d5990b
SebSto91 <s.stordal@gmail.com>
2018-05-11 14:11:04 +0200

    Test to

--- SalesTools/src/ss/demo/salestools/Main.java
+++ SalesTools/src/ss/demo/salestools/Main.java
@@ -18,7 +18,7 @@ public class Main {
 	private static void displayGreeting() {
 		System.out.println("Hello happy sales people");
 		System.out.println("This app shows sales data");
-		System.out.println("Test 1");
+		System.out.println("Test 2");
 	}
 
 }
